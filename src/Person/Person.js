14b0754f087ddb5320d441c2544be947546494f9
Jalees-Jahanzaib <jalees.jahanzaib@students.iiit.ac.in>
2020-01-08 16:47:43 +0530

    10

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,8 @@
 import React from 'react'
-
+import "./Person.css";
 const person = (props) => {
     return(
-<div>
+<div className="Person"> 
     
     <p onClick={props.click}> My name is { props.name} and I m {props.age} years Old! </p>
     
